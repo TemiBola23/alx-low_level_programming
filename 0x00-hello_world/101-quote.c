6ee6092f0a0b13d3635d0ab7956bf0fed0a7cc01
TemiBola23 <temitopebolatito21@gmail.com>
2023-02-19 05:46:03 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - A c program that print a line to the standard error
- * Return: 1 (Success)
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
  */
 int main(void)
-{	
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
-	return (1);
+{
+  write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+  return (1);
 }
