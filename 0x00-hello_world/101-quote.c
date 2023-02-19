ad6725b4aa0d7cb3f5a58726ab3dd537bc8a1666
TemiBola23 <temitopebolatito21@gmail.com>
2023-02-19 05:20:42 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,6 @@
  */
 int main(void)
 {	
-	write(2, "and that piece of art is useful\" - ", 36);
-    	write(2, "Dora Korpar, 2015-10-19\n", 24);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 24);
 	return (1);
 }
