33d3dd95d6f90f6ac685cce0edaf9fbfc85f231b
TemiBola23 <temitopebolatito21@gmail.com>
2023-02-19 13:58:08 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,13 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	
+{
 	int i;
 
 	for (i = 0; i < 26; i++)
-       	{	
+	{
 		putchar("abcdefghijklmnopqrstuvwxyz"[i]);
- 	}	
+	}
 	putchar('\n');
 	return (0);
 }
