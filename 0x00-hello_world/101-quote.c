c7009d1107266dba66da862764098ad3c5af7032
TemiBola23 <temitopebolatito21@gmail.com>
2023-02-19 15:06:58 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,8 @@
  * Return: Always 1 (Error)
  */
 int main(void)
-{	
-      write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+{
+	fput("and that piece of art is useful\"\ \
+			- Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
