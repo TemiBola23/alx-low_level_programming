954dd57e9d4b1dc4a3bfd5d8176b6aefba82266c
TemiBola23 <temitopebolatito21@gmail.com>
2023-02-20 09:14:14 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,19 +7,19 @@
  */
 int main(void)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-	for (j = i; j < 10; j++)
-	{
-	putchar(i + '0');
-	putchar(',');
-	putchar(' ');
-	putchar(j + '0');
-	putchar(j == 9 ? '\n' : ' ');
-	}
-	}
+		putchar(i + 48);
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
 
+		}
+
+	}
+	putchar('\n');
 	return (0);
 }
