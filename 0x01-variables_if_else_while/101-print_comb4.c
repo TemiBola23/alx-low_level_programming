39d65340edfe78590f11f3952298fb5a11c1a909
TemiBola23 <temitopebolatito21@gmail.com>
2023-02-20 15:00:34 -0800

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,41 +1,33 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: write a program that prints all posible
- * different combination of two digits
- * Numbers must be separated by ,, followed by a space
- * The two digits must be different
- * You are not allowed to use any variable of type char
- * All your code should be in the main function
+ * Description: A program that meets all the requirements
+ * The combination of numbers must be separated by comma, followed by a space
+ * different combination of digits
  * Return: 0
  */
 int main(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 7; i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (j = i+1; j < 8; j++)
-		{	
-			for (k = j+1; k < 9; k++)
-			{	
-			int smallest = i*100 + j*10 + k;
-				if (smallest == 120 || smallest == 102 || smallest == 201 || smallest == 210 || smallest == 021 || smallest == 012)
+		for (j = i + 1; j < 9; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+				if (i != j && i != k && j != k)
 				{
-				continue;
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					if (i + j + k < 24)
 					{
-						putchar(i+'0');
-						putchar(j+'0');
-						putchar(k+'0');
-						if (i < 6)
-						{
-							putchar(',');
-							putchar(' ');
-						}
+						putchar(',');
+						putchar(' ');
 					}
 				}
-			}
-		}		
-		return 0;
+		}
+	}
+	putchar('\n');
+	return (0);
 }
-
