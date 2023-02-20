345b4cba8fb550d0bf65f71d2bb45a74e7237c36
TemiBola23 <temitopebolatito21@gmail.com>
2023-02-20 00:46:45 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
-
+/**
+ * main - main block
+ * Description: write a program that prints all posible
+ * different combinations of the digits
+ * number must be seperated by ,, followed space
+ * All your code should be in the main function
+ * Return: 0
+ */
 int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 9; i++) 
+	for (i = 0; i < 9; i++)
 	{
 		for (j = i+1; j <= 9; j++)
 	       	{
 		       	putchar(i+'0');
 			putchar(j+'0');
-			
+
 			if (i < 8);
 			{
 				putchar(',');
