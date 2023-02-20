0f95e1819866b430980431bdb40c05976c3b420a
TemiBola23 <temitopebolatito21@gmail.com>
2023-02-20 06:36:45 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: A program that meets all the requirements
+ * The combination of numbers must be separated by comma, followed by a space
+ * different combination of digits
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j;
+	for (i = 0; i < 100; i++)
+	{
+	for (j = i; j < 100; j++)
+	{
+		if (i == j)
+		{
+			continue;
+		}
+		int smallest = i*100 + j;
+		int smallest_rev = j*100 + i;
+		if (smallest_rev < smallest)
+		{
+			continue;
+		}
+		putchar(i/10+'0');
+		putchar(i%10+'0');
+		putchar(' ');
+		putchar(j/10+'0');
+		putchar(j%10+'0');
+		if (i < 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+	}
+	}
+	return (0);
+}
