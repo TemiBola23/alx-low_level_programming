aa6e46230a482c56e39b95c075da8057585d066f
TemiBola23 <temitopebolatito21@gmail.com>
2023-02-23 07:16:53 -0800

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,31 +9,32 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-	_putchar('0');
-	_putchar(',');
-	_putchar(' ');
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
 
 	for (j = 1; j <= 9; j++)
 	{
-	result = i * j;
+		result = i * j;
 
-	if (result <= 9)
-	{
-	_putchar(' ');
-	_putchar(result + '0');
-	}
-	else
-	{
-	_putchar(' ');
-	_putchar(result / 10 + '0');
-	_putchar(result % 10 + '0');
-	}
+		if (result <= 9)
+		{
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(result + '0');
+		}
+		else
+		{
+			_putchar(' ');
+			_putchar(result / 10 + '0');
+			_putchar(result % 10 + '0');
+		}
 
-	if (j != 9)
-	{
-	_putchar(',');
-	_putchar(' ');
-	}
+		if (j != 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
 	}
 	_putchar('\n');
 	}
