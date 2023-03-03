c07e8826cf3dd978a5699b147ab9e59643fa3e3b
TemiBola23 <temitopebolatito21@gmail.com>
2023-03-02 19:34:24 -0800

    102-magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Description: This program modifies the value of an integer variable
- *              indirectly through a pointer to print a specific value.
- *              Only one statement is allowed and the variable 'a' cannot
- *              be used or modified.
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-        int n;
-        int a[5];
-        int *p;
+	int n;
+	int a[5];
+	int *p;
 
-        a[2] = 1024;
-        p = &n;
-
-        *(p + 5) = 98;
-
-/* ...so that this prints 98\n */
-        printf("a[2] = %d\n", a[2]);
-        return (0);
+	a[2] = 1024;
+	p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+	*(p + 5) = 98;
+  /* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
