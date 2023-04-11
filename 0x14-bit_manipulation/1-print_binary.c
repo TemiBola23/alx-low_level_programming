4cddfe8aa6e76562027e869439b2d761f968db22
TemiBola23 <temitopebolatito21@gmail.com>
2023-04-10 17:56:50 -0700

    1-print_binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: the number to print
+ */
+void print_binary(unsigned long int n)
+{
+	int i, count = 0;
+	unsigned long int current;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	for (i = 63; i >= 0; i--)
+	{
+	current = n >> i;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+		_putchar('0');
+	}
+}
